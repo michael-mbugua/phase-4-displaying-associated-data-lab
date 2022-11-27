b66e2ea7331cf21b0a7419c735fd9e57138eae60
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-27 15:40:35 +0300

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def show
+        user=User.find(params[:id])
+        render json: user, include: :items
+        
+    end
+    def index
+        user=User.all
+        render json: user ,status: :ok
+    end
 end
